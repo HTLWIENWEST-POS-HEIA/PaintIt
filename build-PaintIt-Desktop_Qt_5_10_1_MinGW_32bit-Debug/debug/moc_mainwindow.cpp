3907b0940b3b243a1f75966e121bec5acb23bf71
Dominic Resch <dominic.resch@chello.at>
2018-05-27 11:46:51 +0200

    Hotfix
    Es gab ein Problem mit den MakeFiles weshalb das Programmieren nicht zum Kompilieren ging, sollte nun aber funktionieren

--- build-PaintIt-Desktop_Qt_5_10_1_MinGW_32bit-Debug/debug/moc_mainwindow.cpp
+++ build-PaintIt-Desktop_Qt_5_10_1_MinGW_32bit-Debug/debug/moc_mainwindow.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[16];
-    char stringdata0[333];
+    QByteArrayData data[19];
+    char stringdata0[398];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -46,7 +46,10 @@ QT_MOC_LITERAL(11, 220, 30), // "on_tbtnBackgroundColor_clicked"
 QT_MOC_LITERAL(12, 251, 30), // "on_spinBox_Radius_valueChanged"
 QT_MOC_LITERAL(13, 282, 6), // "radius"
 QT_MOC_LITERAL(14, 289, 33), // "on_spinBox_LineWidth_valueCha..."
-QT_MOC_LITERAL(15, 323, 9) // "lineWidth"
+QT_MOC_LITERAL(15, 323, 9), // "lineWidth"
+QT_MOC_LITERAL(16, 333, 19), // "on_tbtnRect_clicked"
+QT_MOC_LITERAL(17, 353, 22), // "on_tbtnEllipse_clicked"
+QT_MOC_LITERAL(18, 376, 21) // "on_tbtnEraser_clicked"
 
     },
     "MainWindow\0on_tbtnLine_clicked\0\0"
@@ -58,7 +61,8 @@ QT_MOC_LITERAL(15, 323, 9) // "lineWidth"
     "on_tbtnBackgroundColor_clicked\0"
     "on_spinBox_Radius_valueChanged\0radius\0"
     "on_spinBox_LineWidth_valueChanged\0"
-    "lineWidth"
+    "lineWidth\0on_tbtnRect_clicked\0"
+    "on_tbtnEllipse_clicked\0on_tbtnEraser_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -68,7 +72,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      12,   14, // methods
+      15,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -76,18 +80,21 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   74,    2, 0x08 /* Private */,
-       3,    0,   75,    2, 0x08 /* Private */,
-       4,    0,   76,    2, 0x08 /* Private */,
-       5,    0,   77,    2, 0x08 /* Private */,
-       6,    0,   78,    2, 0x08 /* Private */,
-       7,    0,   79,    2, 0x08 /* Private */,
-       8,    0,   80,    2, 0x08 /* Private */,
-       9,    0,   81,    2, 0x08 /* Private */,
-      10,    0,   82,    2, 0x08 /* Private */,
-      11,    0,   83,    2, 0x08 /* Private */,
-      12,    1,   84,    2, 0x08 /* Private */,
-      14,    1,   87,    2, 0x08 /* Private */,
+       1,    0,   89,    2, 0x08 /* Private */,
+       3,    0,   90,    2, 0x08 /* Private */,
+       4,    0,   91,    2, 0x08 /* Private */,
+       5,    0,   92,    2, 0x08 /* Private */,
+       6,    0,   93,    2, 0x08 /* Private */,
+       7,    0,   94,    2, 0x08 /* Private */,
+       8,    0,   95,    2, 0x08 /* Private */,
+       9,    0,   96,    2, 0x08 /* Private */,
+      10,    0,   97,    2, 0x08 /* Private */,
+      11,    0,   98,    2, 0x08 /* Private */,
+      12,    1,   99,    2, 0x08 /* Private */,
+      14,    1,  102,    2, 0x08 /* Private */,
+      16,    0,  105,    2, 0x08 /* Private */,
+      17,    0,  106,    2, 0x08 /* Private */,
+      18,    0,  107,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -102,6 +109,9 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void,
     QMetaType::Void, QMetaType::Int,   13,
     QMetaType::Void, QMetaType::Int,   15,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
 
        0        // eod
 };
@@ -124,6 +134,9 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 9: _t->on_tbtnBackgroundColor_clicked(); break;
         case 10: _t->on_spinBox_Radius_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
         case 11: _t->on_spinBox_LineWidth_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 12: _t->on_tbtnRect_clicked(); break;
+        case 13: _t->on_tbtnEllipse_clicked(); break;
+        case 14: _t->on_tbtnEraser_clicked(); break;
         default: ;
         }
     }
@@ -154,13 +167,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 12)
+        if (_id < 15)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 12;
+        _id -= 15;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 12)
+        if (_id < 15)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 12;
+        _id -= 15;
     }
     return _id;
 }

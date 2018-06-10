27f2deb3c4b77ce0924f11379dbb83037b9dc2e8
Markus <nessel.m87@htl-ottakring.ac.at>
2018-06-10 20:57:18 +0200

    Linetool with color
    Linetool works.
    Added primary & secondary color Linetool

--- PaintIt/Tools/mylinetool.cpp
+++ PaintIt/Tools/mylinetool.cpp
@@ -1,9 +1,40 @@
 #include "mylinetool.h"
 
 QGraphicsItem *MyLineTool::mousePressed(QGraphicsSceneMouseEvent *event, MyToolProperties *myToolProperties) {
+
+    if(clickCounter == 0) {
+        clickCounter ++;
+        p1 = event->scenePos().toPoint();
+    }
+    //Use Primary Color
+    else if (clickCounter == 1 && event->button() == Qt::LeftButton) {
+        clickCounter = 0;
+        p2 = event->scenePos().toPoint();
+        myLine = new QLine();
+        myLine->setP1(p1);
+        myLine->setP2(p2);
+        myLineItem = new QGraphicsLineItem(*myLine);
+        myLineItem->setPen(myToolProperties->myPrimaryPen.color());
+        return (QGraphicsItem*)myLineItem;
+        //QMessageBox::information(0,"Information",QString::number(p1.x()) + ", " + QString::number(p1.y()) + " ---- " + QString::number(p2.x()) + ", " + QString::number(p2.y()),QMessageBox::Ok);
+    }
+    //Use Secondary Color
+    else if (clickCounter == 1 && event->button() == Qt::RightButton){
+        clickCounter = 0;
+        p2 = event->scenePos().toPoint();
+        myLine = new QLine();
+        myLine->setP1(p1);
+        myLine->setP2(p2);
+        myLineItem = new QGraphicsLineItem(*myLine);
+        myLineItem->setPen(myToolProperties->mySecondaryPen.color());
+        return (QGraphicsItem*)myLineItem;
+        //QMessageBox::information(0,"Information",QString::number(p1.x()) + ", " + QString::number(p1.y()) + " ---- " + QString::number(p2.x()) + ", " + QString::number(p2.y()),QMessageBox::Ok);
+    }
+
     return nullptr;
 }
 
 void MyLineTool::mouseMoved(QGraphicsSceneMouseEvent *event) {
-
+    //myLine->setP2(event->lastScenePos().toPoint());
+    //myLineItem->setLine(myLine);
 }
--- PaintIt/Tools/mylinetool.h
+++ PaintIt/Tools/mylinetool.h
@@ -2,11 +2,19 @@
 #define MYLINETOOL_H
 
 #include "toolbase.h"
+#include <QLine>
+#include <QGraphicsLineItem>
+#include <QMessageBox>
 
 class MyLineTool : public ToolBase {
 public:
     QGraphicsItem *mousePressed(QGraphicsSceneMouseEvent *event, MyToolProperties *myToolProperties);
     void mouseMoved(QGraphicsSceneMouseEvent *event);
+private:
+    QGraphicsLineItem *myLineItem;
+    QLine *myLine;
+    QPoint p1, p2;
+    int clickCounter = 0;
 };
 
 #endif // MYLINETOOL_H

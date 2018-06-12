53bc417be1767d5f04218add14426b5f904b77ef
Markus <nessel.m87@htl-ottakring.ac.at>
2018-06-12 22:29:48 +0200

    Linetool complete
    Linetool has now changebal linewidth and by holding down after the first click the line is visibel and follows your movemend.

--- PaintIt/Tools/mylinetool.cpp
+++ PaintIt/Tools/mylinetool.cpp
@@ -2,39 +2,43 @@
 
 QGraphicsItem *MyLineTool::mousePressed(QGraphicsSceneMouseEvent *event, MyToolProperties *myToolProperties) {
 
-    if(clickCounter == 0) {
+    if(clickCounter == 0 && (event->button() == Qt::LeftButton || event->button() == Qt::RightButton)) {
         clickCounter ++;
         p1 = event->scenePos().toPoint();
+        myLine = new QLine();
+        myLine->setP1(p1);
+        myLineItem = new QGraphicsLineItem();
     }
     //Use Primary Color
     else if (clickCounter == 1 && event->button() == Qt::LeftButton) {
         clickCounter = 0;
         p2 = event->scenePos().toPoint();
-        myLine = new QLine();
-        myLine->setP1(p1);
         myLine->setP2(p2);
-        myLineItem = new QGraphicsLineItem(*myLine);
+        myLineItem->setLine(*myLine);
         myLineItem->setPen(myToolProperties->myPrimaryPen.color());
-        return (QGraphicsItem*)myLineItem;
         //QMessageBox::information(0,"Information",QString::number(p1.x()) + ", " + QString::number(p1.y()) + " ---- " + QString::number(p2.x()) + ", " + QString::number(p2.y()),QMessageBox::Ok);
     }
     //Use Secondary Color
     else if (clickCounter == 1 && event->button() == Qt::RightButton){
         clickCounter = 0;
         p2 = event->scenePos().toPoint();
-        myLine = new QLine();
-        myLine->setP1(p1);
         myLine->setP2(p2);
-        myLineItem = new QGraphicsLineItem(*myLine);
+        myLineItem->setLine(*myLine);
         myLineItem->setPen(myToolProperties->mySecondaryPen.color());
-        return (QGraphicsItem*)myLineItem;
         //QMessageBox::information(0,"Information",QString::number(p1.x()) + ", " + QString::number(p1.y()) + " ---- " + QString::number(p2.x()) + ", " + QString::number(p2.y()),QMessageBox::Ok);
     }
-
+    if(myLineItem != nullptr && clickCounter == 0){
+        //Temporary pen template created and filled with color from primary or secondary pen to allow editing the line width
+        QPen TempPen = myLineItem->pen();
+        TempPen.setWidth(myToolProperties->myLineWidth);
+        myLineItem->setPen(TempPen);
+        return (QGraphicsItem*)myLineItem;
+    }
     return nullptr;
 }
 
 void MyLineTool::mouseMoved(QGraphicsSceneMouseEvent *event) {
-    //myLine->setP2(event->lastScenePos().toPoint());
-    //myLineItem->setLine(myLine);
+    //To see the line, click once and hold the button
+    myLine->setP2(event->lastScenePos().toPoint());
+    myLineItem->setLine(*myLine);
 }

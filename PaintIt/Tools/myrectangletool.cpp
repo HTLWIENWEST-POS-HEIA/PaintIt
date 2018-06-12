4a4158bc03c36579eb23e0f152193e9cbd54aae5
einfachnurmatt <39101399+einfachnurmatt@users.noreply.github.com>
2018-06-12 17:11:32 +0200

    Rechteck Eingefügt

--- PaintIt/Tools/myrectangletool.cpp
+++ PaintIt/Tools/myrectangletool.cpp
@@ -1,9 +1,67 @@
 #include "myrectangletool.h"
+#include <QMessageBox>
 
 QGraphicsItem *MyRectangleTool::mousePressed(QGraphicsSceneMouseEvent *event, MyToolProperties *myToolProperties) {
+
+
+    if((this->mouseclick==0) && event->button()==Qt::LeftButton){
+        mouseclick++;
+        firstpoint = event->scenePos().toPoint();
+
+       // QMessageBox::information(0,"Information", "Eins", QMessageBox::Ok);
+        rect=new QRect(firstpoint.x(),firstpoint.y(),0,0);
+        rect_item = new QGraphicsRectItem(*rect);
+        rect_item->setPen(myToolProperties->myPrimaryPen);
+        return (QGraphicsItem*)rect_item;
+    }
+
+    else if(this->mouseclick==1 && event->button()==Qt::LeftButton){
+        mouseclick=0;
+        lastpoint = event->scenePos().toPoint();
+       // QMessageBox::information(0,"Information", "Zwei", QMessageBox::Ok);
+       // rect_item->rect().setBottomRight(lastpoint);
+    }
+
+        if(firstpoint.x() > lastpoint.x() && firstpoint.y()>lastpoint.y()){
+            rect->setTopLeft(lastpoint);
+            rect->setBottomRight(firstpoint);
+            rect_item->setRect(*rect);
+
+        }
+        else if(lastpoint.x() > firstpoint.x() && lastpoint.y()>firstpoint.y()){
+            rect->setTopLeft(firstpoint);
+            rect->setBottomRight(lastpoint);
+
+     //   rect->setBottomRight(lastpoint);
+        rect_item->setRect(*rect);
+    }
+
+
+
     return nullptr;
 }
 
 void MyRectangleTool::mouseMoved(QGraphicsSceneMouseEvent *event) {
 
+ /* if((event->buttons() && Qt::LeftButton)) {
+           lastpoint = event->lastScenePos().toPoint();
+
+           if(firstpoint.x() < lastpoint.x() && firstpoint.y() < lastpoint.y()) {
+               rect->setTopLeft(firstpoint);
+               rect->setBottomRight(lastpoint);
+           }
+           else if(firstpoint.x() < lastpoint.x() && firstpoint.y() > lastpoint.y()) {
+               rect->setBottomLeft(firstpoint);
+               rect->setTopRight(lastpoint);
+           }
+           else if(firstpoint.x() > lastpoint.x() && firstpoint.y() < lastpoint.y()) {
+               rect->setTopRight(firstpoint);
+              rect->setBottomLeft(lastpoint);
+           }
+           else {
+               rect->setBottomRight(firstpoint);
+               rect->setTopLeft(lastpoint);
+           }
+           rect_item->setRect(*(rect));
+       }*/
 }
--- PaintIt/Tools/myrectangletool.h
+++ PaintIt/Tools/myrectangletool.h
@@ -1,12 +1,21 @@
 #ifndef MYRECTANGLETOOL_H
 #define MYRECTANGLETOOL_H
 
+#include <QRect>
 #include "toolbase.h"
 
 class MyRectangleTool : public ToolBase {
 public:
     QGraphicsItem *mousePressed(QGraphicsSceneMouseEvent *event, MyToolProperties *myToolProperties);
     void mouseMoved(QGraphicsSceneMouseEvent *event);
+
+private:
+    QPoint lastpoint;
+    QPoint firstpoint;
+    int mouseclick = 0;
+    QRect *rect;
+    QGraphicsRectItem *rect_item;
+
 };
 
 #endif

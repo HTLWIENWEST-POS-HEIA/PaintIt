8bc08bb4953a12392b57dea10c37045721fbec3d
teker-a93 <39065004+teker-a93@users.noreply.github.com>
2018-06-18 19:05:17 +0200

    akalin.Circletool

--- PaintIt/Tools/mycircletool.cpp
+++ PaintIt/Tools/mycircletool.cpp
@@ -1,9 +1,23 @@
 #include "mycircletool.h"
 
 QGraphicsItem *MyCircleTool::mousePressed(QGraphicsSceneMouseEvent *event, MyToolProperties *myToolProperties) {
+    if(event->button() == Qt::LeftButton) {
+        firstPoint = event->lastScenePos().toPoint();
+        myRect = new QRect(firstPoint.x(), firstPoint.y(), 0, 0);
+        myEllipseItem = new QGraphicsEllipseItem(*(myRect));
+        QPen myPen = myToolProperties->myPrimaryPen;
+        myPen.setWidth(myToolProperties->myLineWidth);
+        myEllipseItem->setPen(myPen);
+        myEllipseItem->setBrush(myToolProperties->mySecondaryPen.color());
+        return (QGraphicsItem*)myEllipseItem;
+    }
     return nullptr;
 }
 
 void MyCircleTool::mouseMoved(QGraphicsSceneMouseEvent *event) {
-
+    if((event->buttons() && Qt::LeftButton)) {
+        lastPoint = event->lastScenePos().toPoint();
+        myRect->setBottomRight(lastPoint);
+        myEllipseItem->setRect(*(myRect));
+    }
 }
--- PaintIt/Tools/mycircletool.h
+++ PaintIt/Tools/mycircletool.h
@@ -1,12 +1,19 @@
 #ifndef MYCIRCLETOOL_H
 #define MYCIRCLETOOL_H
 
+#include <QRect>
 #include "toolbase.h"
+#include <QGraphicsEllipseItem>
 
 class MyCircleTool : public ToolBase {
 public:
     QGraphicsItem *mousePressed(QGraphicsSceneMouseEvent *event, MyToolProperties *myToolProperties);
     void mouseMoved(QGraphicsSceneMouseEvent *event);
+
+private:
+    QRect *myRect;
+    QGraphicsEllipseItem *myEllipseItem;
+    QPoint lastPoint, firstPoint;
 };
 
 #endif
